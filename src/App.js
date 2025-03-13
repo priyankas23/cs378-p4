af312d930adedb88c1776787c16a561762814373
Priyanka Sadagopan <priyankasadagopan@Priyankas-MacBook-Pro.local>
2025-03-12 23:42:42 -0500

    AUGH

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+
+
+const headerImage = {
+  logo: 'INPLANESIGHTLOGO.PNG';
+}
+
+export const HeaderItems = ({logo}) => {
+  return(
+    <Container fluid> 
+      <Col sm={{span: 4}}>
+      <div className = "headerimg">
+       <img src= {(process.env.PUBLIC_URL+"/images/"+logo)}></img>
+      </div>
+    </Col>
+    </Container>
+);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "headerImage">
+        <HeaderItems logo = {headerImage.logo}></HeaderItems>
+      </div>
+      <h1>me: oh seventeen has all bangers except for power of love </h1>
+      <h1>my playlist:fuck you (plays power of love) </h1>
     </div>
   );
 }
