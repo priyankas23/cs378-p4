a43b31d11f521be1fb344cd7e4caf5c9185502af
Priyanka Sadagopan <priyankasadagopan@Priyankas-MacBook-Pro.local>
2025-03-12 23:51:22 -0500

    AUGH

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+
 
 
 const headerImage = {
-  logo: 'INPLANESIGHTLOGO.PNG';
+  logo: 'INPLANESIGHTLOGO.PNG',
 }
 
 export const HeaderItems = ({logo}) => {
@@ -13,12 +16,34 @@ export const HeaderItems = ({logo}) => {
     <Container fluid> 
       <Col sm={{span: 4}}>
       <div className = "headerimg">
-       <img src= {(process.env.PUBLIC_URL+"/images/"+logo)}></img>
+       <img src= {(process.env.PUBLIC_URL+'/images/'+logo)}></img>
       </div>
     </Col>
     </Container>
 );
 }
+
+
+export function SearchBox() {
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchText}
+        onChange={handleInputChange}
+      />
+      <p>Searching for: {searchText}</p>
+    </div>
+  );
+}
+
 
 function App() {
   return (
@@ -26,8 +51,11 @@ function App() {
       <div className = "headerImage">
         <HeaderItems logo = {headerImage.logo}></HeaderItems>
       </div>
-      <h1>me: oh seventeen has all bangers except for power of love </h1>
-      <h1>my playlist:fuck you (plays power of love) </h1>
+      <h1>MY AIRPORTS </h1>
+      <Button> DFW </Button>
+      <Button> SFO </Button>
+      <Button> AUS </Button>
+      <SearchBox></SearchBox>
     </div>
   );
 }
